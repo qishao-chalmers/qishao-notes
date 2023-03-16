9a397bceedfc09fbe9a7325cebeac3a54756412b
hitqshao <shaoqi0401@163.com>
2023-03-16 18:08:59 +0100

    first modify

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,14 +4,30 @@ const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
-  title: "vdoing-template",
+  title: "qishao-notes",
   description: 'vdoing博客主题模板',
-  base: '/vdoing-template/', // 格式：'/<仓库名>/'， 默认'/'
+  base: '/qishao-notes/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
   },
 
   head,
-  plugins,
+  // vssue 评论插件
+  plugins: [
+    [
+      "vuepress-plugin-vssue-global",
+      {
+        platform: "github",
+        title: "[Comment]<%- frontmatter.title %>",
+        needComments: true,
+        // 其他的 Vssue 配置
+        autoCreateIssue: true,
+        clientId: "bf6ab1891433c9f0a781",
+        clientSecret: "6dc23551d5b49b91364fb55d0ab5163c3f5cd9e0",
+        owner: "hitqshao",
+        repo: "qishao-notes",
+      },
+    ],
+  ],
   themeConfig,
 }
