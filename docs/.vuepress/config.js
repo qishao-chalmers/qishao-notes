3f2a604ae3a35f86084471e011f648378d9d468c
hitqshao <shaoqi0401@163.com>
2023-03-16 18:16:44 +0100

    reverse comment plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,21 +13,6 @@ module.exports = {
 
   head,
   // vssue 评论插件
-  plugins: [
-    [
-      "vuepress-plugin-vssue-global",
-      {
-        platform: "github",
-        title: "[Comment]<%- frontmatter.title %>",
-        needComments: true,
-        // 其他的 Vssue 配置
-        autoCreateIssue: true,
-        clientId: "bf6ab1891433c9f0a781",
-        clientSecret: "6dc23551d5b49b91364fb55d0ab5163c3f5cd9e0",
-        owner: "hitqshao",
-        repo: "qishao-notes",
-      },
-    ],
-  ],
+  plugins,
   themeConfig,
 }
