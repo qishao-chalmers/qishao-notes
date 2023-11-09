0721e10408bf8fa7e5aaf8d4eac97cb385086c1d
QiShao <shaoqi0401@163.com>
2023-11-09 15:57:18 +0100

    change name of directory

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -4,6 +4,7 @@ module.exports = [
   { text: 'one',link: '/one/',},
   { text: 'two',link: '/two/',},
   { text: 'gpu', link: '/gpu/' },
+  { text: 'cpu', link: '/cpu/' },
   { text: 'BBS', link: '/message-board/' },
   { text: 'CSDN', link: 'https://blog.csdn.net/hit_shaoqi' },
 ]
