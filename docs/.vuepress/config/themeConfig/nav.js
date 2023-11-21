c66cb72afefe1e3006d4021b36f24f703114ad60
QiShao <shaoqi0401@163.com>
2023-11-21 23:20:03 +0100

    Update nav.js

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -2,6 +2,7 @@
 module.exports = [
   { text: 'Home', link: '/' },
   { text: 'hbm',link: '/hbm/',},
+  { text: 'compiler', link: '/compiler/' },
   { text: 'gpu', link: '/gpu/' },
   { text: 'cpu', link: '/cpu/' },
   { text: 'BBS', link: '/message-board/' },
