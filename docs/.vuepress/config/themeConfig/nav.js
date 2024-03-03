bc6601034c2875f4bda9b19712b2c262cb03ad54
QiShao <shaoqi0401@163.com>
2024-03-03 13:25:48 +0100

    Update nav.js

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -6,6 +6,7 @@ module.exports = [
   { text: 'gpu', link: '/gpu/' },
   { text: 'cpu', link: '/cpu/' },
   { text: 'llm', link: '/llm/' },
+  { text: 'unix', link: '/unix/' },
   { text: 'BBS', link: '/message-board/' },
   { text: 'CSDN', link: 'https://blog.csdn.net/hit_shaoqi' },
 ]
